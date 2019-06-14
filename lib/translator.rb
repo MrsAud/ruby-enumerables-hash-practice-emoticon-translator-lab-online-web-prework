2ec71ed3aa369d67aa851064f86f94d2c52ffa4b
Audrea Cook <audreacwork@gmail.com>
2019-06-14 20:38:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if japanese_emoticon 
     return japanese_emoticon
   else 
-    return "Sorry, that emoticon was not found."
+    return "Sorry, that emoticon was not found"
   end
   
 end
