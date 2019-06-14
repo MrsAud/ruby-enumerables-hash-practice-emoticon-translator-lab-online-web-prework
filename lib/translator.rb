2bf8efed9ac8f16abbcd272076b7cd8b49bb9d60
Audrea Cook <audreacwork@gmail.com>
2019-06-14 20:11:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,7 @@ require "yaml"
 def load_library(yml_file)
   emote = YAML.load_file(yml_file)
   emote = {}
-  emote[:get_emoticon] = ""
-  emote[:get_meaning] = ""
-end
+  end
 
 def get_japanese_emoticon
   # code goes here
