83a237c3dc90346a2fe786b415d26fd2c9355c00
Audrea Cook <audreacwork@gmail.com>
2019-06-14 20:40:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,6 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
   
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  english_meaning = load_library(file_path)["get_meaning"][emoticon]
+  english_meaning
 end
